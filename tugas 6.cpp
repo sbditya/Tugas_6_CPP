4aec50644f030cc4c566666f2a00d8db67fed2cd
Salsabila Ditya Anisa <sbditya320@gmail.com>
2021-09-01 13:06:22 +0700

    Tugas 6 CPP Niomic
    Salsabila Ditya Anisa

---
+++ tugas 6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std; 
+
+int main(){
+	int x,z;
+	cout<<"Masukan Nilai X : ";
+	cin>>x;
+	cout<<"Masukan Nilai Z : ";
+	cin>>z; 
+	
+	if(x>=z){
+		cout<<"25";
+	}else{
+		cout<<"-15";
+	}
+}
